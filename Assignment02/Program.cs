d546ad574ace40f06c5b394d490b489a9f9e98b5
Luis Alberto Garnica López <lagl95@hotmail.com>
2020-08-29 19:48:10 -0500

    Prueba de concepto sobre ML.NET

---
+++ Assignment02/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+
+namespace Assignment02
+{
+    class Program
+    {
+        static readonly string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "dataset2.arff");
+        static readonly string _modelPath = Path.Combine(Environment.CurrentDirectory, "Data", "IrisClusteringModel.zip");
+        static void Main(string[] args)
+        {
+            var mlContext = new MLContext(seed: 0);
+            IDataView dataView = mlContext.Data.LoadFromTextFile<Data>(_dataPath, hasHeader: false, separatorChar: ',');
+
+            string featuresColumnName = "Features";
+            var pipeline = mlContext.Transforms
+                .Concatenate(featuresColumnName, "SepalLength", "SepalWidth", "PetalLength", "PetalWidth")
+                .Append(mlContext.Clustering.Trainers.KMeans(featuresColumnName, numberOfClusters: 2));
+
+            var model = pipeline.Fit(dataView);
+
+            using (var fileStream = new FileStream(_modelPath, FileMode.Create, FileAccess.Write, FileShare.Write))
+            {
+                mlContext.Model.Save(model, dataView.Schema, fileStream);
+            }
+
+            var predictor = mlContext.Model.CreatePredictionEngine<Data, ClusterPrediction>(model);
+
+            var prediction = predictor.Predict(TestData.Setosa);
+            Console.WriteLine($"Cluster: {prediction.PredictedClusterId}");
+            Console.WriteLine($"Distances: {string.Join(" ", prediction.Distances)}");
+        }
+    }
+}
---
+++ Assignment02/TestData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment02
+{
+    static class TestData
+    {
+        internal static readonly Data Setosa = new Data
+        {
+            SepalLength = 5.1f,
+            SepalWidth = 3.5f,
+            PetalLength = 1.4f,
+            PetalWidth = 0.2f
+        };
+    }
+}
